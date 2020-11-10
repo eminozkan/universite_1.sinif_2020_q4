59d715d872b40ad689d2c9907d315aa7361b5492
Muhammet Emin Özkan <70413758+eminozkan@users.noreply.github.com>
2020-11-10 15:57:21 +0300

    Create gcd-lcm.cpp

---
+++ 2./gcd-lcm.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+struct numbers{
+    int no1;
+    int no2;
+};
+
+int main() {
+    numbers startnumber;
+    cout << "OBEB ve OKEK lerini hesaplamak istediginiz sayilari giriniz: ";
+    cin >> startnumber.no1 >> startnumber.no2;
+
+
+    numbers okek;
+    okek.no1 = startnumber.no1;
+    okek.no2 = startnumber.no2;
+
+    numbers obeb;
+    obeb.no1 = startnumber.no1;
+    obeb.no2 = startnumber.no2;
+
+    //OBEB
+    while(obeb.no1 % obeb.no2 != 0 && obeb.no2 % obeb.no1 != 0){
+        if (obeb.no1>obeb.no2) {
+            obeb.no1 = obeb.no1 % obeb.no2;
+        }else{
+            obeb.no2= obeb.no2 % obeb.no1;
+        }
+    }
+    int obebnumbers = (obeb.no1 < obeb.no2 ? obeb.no1 : obeb.no2);
+
+    cout << "OBEB(" << startnumber.no1 << "," << startnumber.no2 << ") = " << obebnumbers << endl;
+
+    // OKEK
+
+    while(okek.no1 != okek.no2){
+        if(okek.no1<okek.no2){
+            okek.no1 += startnumber.no1;
+        }else{
+            okek.no2 += startnumber.no2;
+        }
+    }
+
+    cout << "OKEK(" << startnumber.no1 << "," << startnumber.no2 << ") = " << okek.no2 << endl;
+
+    return 0;
+}
